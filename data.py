d43aa1c1a566bd65fe86e2102a1c5c1c731b1b46
Dylan Vener <dvener@blitzle.soic.indiana.edu>
2017-07-16 17:00:00 -0400

    fixed up some final bugs stopping it from running

--- data.py
+++ data.py
@@ -14,20 +14,21 @@ class ImdbDataset(Dataset):
 
     def __len__(self):
         return len(self.pairs)
-    
+
     def __getitem__(self, idx):
         X_path = os.path.join(self.root_dir, self.pairs[idx][0])
         X = torch.load(X_path)
         if self.avgpool:
-            X = F.max_pool2d(X, kernel_size=X.size()[2:])
+            X = F.max_pool2d(Variable(X), kernel_size=X.size()[2:])
         else:
-            X = F.max_pool2d(X, kernel_size=X.size()[2:])
+            X = F.max_pool2d(Variable(X), kernel_size=X.size()[2:])
 
-        X = X.view(-1, X.size(1)*X.size(2)*X.size(3))
+        X = X.data
+        X = X.view(X.size(1)*X.size(2)*X.size(3))
 
-        y = torch.FloatTensor([self.pairs[idx][1]])
+        y = torch.LongTensor([self.pairs[idx][1]])[0]
 
         return {'X' : X, 'y' : y}
 
-    
+
 
--- train.py
+++ train.py
@@ -4,11 +4,15 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from tqdm import tqdm
+import sys
 
 from models import Sex
 from data import ImdbDataset
 
-dataset = ImdbDataset('./current_ftg.json','')
+
+label_file = sys.argv[1]
+root_dir = sys.argv[2]
+dataset = ImdbDataset(label_file, root_dir)
 loader = DataLoader(dataset, batch_size = 4, shuffle=True, num_workers = 2, pin_memory = True)
 
 model = Sex().cuda()
@@ -19,15 +23,17 @@ optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 epoch = tqdm(enumerate(loader))
 
 for cnt, data in epoch:
-    X, label = data['X'], data['label']
+    X, label = data['X'], data['y']
     X, label = Variable(X.cuda()), Variable(label.cuda())
 
     optimizer.zero_grad()
 
     output = model(X)
+    print(output)
+    print(label)
     loss = criterion(output, label)
     optimizer.step()
-    
+
     if cnt % 1000 == 999:
         epoch.write(loss.data)
 
